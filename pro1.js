8504cee6e0bab61f0921917a106b495b618351b8
RiteshKr001 <85997459+RiteshKr001@users.noreply.github.com>
2025-04-01 12:36:13 +0000

    check2

---
+++ pro1.js
@@ -0,0 +1,38 @@
+console.log("hello");
+
+const accId = 1223222; // cannot be changed its not allowed
+let accEmail = "353@gmail.com"; //can be changed
+var accPass = "2345" // forget about var due to scope issue only const and let are being used
+//prefer not to use var due to block scope and functional scope issue
+accountCity = "delhi"
+let accountState;
+
+// console.table([accEmail, accId, accPass, accountState])
+
+"use strict";  //treat all JS code as newer version
+
+//code readability should be high
+
+//DATATYPE
+
+let name = "ritesh"
+let age = 20
+let isActive = true;
+
+//Primitive data type
+
+// num = 2^53
+//bigint
+//string -> ""
+//boolean ->true/false
+//null -> standalone value
+//undefined -> 
+//symbol 
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+//object
+
+
+
